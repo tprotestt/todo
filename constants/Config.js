b656e7c85f8fc9c22afe3507ec765e7650ba0720
tprotestt <37515324+tprotestt@users.noreply.github.com>
2018-03-18 17:54:16 -0400

    Uploading the ground floor

---
+++ constants/Config.js
@@ -0,0 +1,7 @@
+// all the app configurations
+export default {
+    url: 'https://CU7G8TOqR:fa2c9efd-f49c-4176-8352-d054ed6cd6cc@scalr.api.appbase.io',
+    app: 'todos-native',
+    credentials: 'Compass15',
+    type: 'todos-native',
+};
